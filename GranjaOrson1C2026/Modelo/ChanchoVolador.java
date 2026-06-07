3cf2e2464ca24875053ab45260e7073c6c8f8bbd
morta <jolfernandez@fi.uba.ar>
2026-06-07 19:45:02 -0300

    agrego archivos

---
+++ GranjaOrson1C2026/Modelo/ChanchoVolador.java
@@ -0,0 +1,27 @@
+package Modelo;
+
+public class ChanchoVolador {
+    private Accion volar;
+
+    //constructor
+
+
+
+    //metodos
+
+    public int calcularEnergia(){
+        return 1;
+
+
+    }
+
+    public void aletear(int veces){
+
+    }
+
+
+
+
+
+    
+}
---
+++ GranjaOrson1C2026/Modelo/Volar.java
@@ -0,0 +1,18 @@
+package Modelo;
+
+public class Volar implements Accion {
+
+    private int costo;
+
+    //constructor
+
+
+
+    //metodos
+
+    public void aletear(int veces){
+
+    }
+
+
+}
